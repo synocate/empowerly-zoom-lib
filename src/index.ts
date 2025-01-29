fbb4c4e616c35a94be9c4bd254a55214736b8d85
Denimar <denimar@gmail.com>
2025-01-29 19:11:45 -0300

    Implementing CI

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import createZoomMeeting from './createZoomMeeting'
-import cancelZoomMeeting from './cancelZoomMeeting'
-import deleteZoomMeeting from './deleteZoomMeeting'
-
-export default {
-  createZoomMeeting,
-  cancelZoomMeeting,
-  deleteZoomMeeting
-}
+export { default } from './EmpowerlyZoomLib'
--- src/utils/getZoomToken.ts
+++ src/utils/getZoomToken.ts
@@ -1,15 +1,12 @@
 import axios from "axios";
 
-export const getZoomToken = async () => {
+export const getZoomToken = async (zoomAccountId: string, zoomClientId: string, zoomClientSecret: string) => {
   const ZOOM_OAUTH_ENDPOINT = 'https://zoom.us/oauth/token';
-  const ZOOM_ACCOUNT_ID = 'DT0wDLTbRzWk3ry-j-HDSQ' //process.env.ZOOM_ACCOUNT_ID
-  const ZOOM_CLIENT_ID = 'Cmkm6Y_QStG6N0m6ZGgCzg' //process.env.ZOOM_CLIENT_ID
-  const ZOOM_CLIENT_SECRET = 'dyinXEM29NuiBZVAVm8CFPCpHU8aZVDQ' //process.env.ZOOM_CLIENT_SECRET
 
   const request = await axios.post(
     ZOOM_OAUTH_ENDPOINT,
-    `account_id=${ZOOM_ACCOUNT_ID}&grant_type=account_credentials`,
-    { headers: { 'Authorization': `Basic ${Buffer.from(`${ZOOM_CLIENT_ID}:${ZOOM_CLIENT_SECRET}`).toString('base64')}` } }
+    `account_id=${zoomAccountId}&grant_type=account_credentials`,
+    { headers: { 'Authorization': `Basic ${Buffer.from(`${zoomClientId}:${zoomClientSecret}`).toString('base64')}` } }
   );
   return request.data.access_token;
 }
